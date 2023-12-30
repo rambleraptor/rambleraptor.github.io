6ccd0dea0ec0e05749f6c7b234990873d6169d5f
Alex Stephen <astephen2@gmail.com>
2023-12-30 13:09:13 -0800

    prettier

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { z, defineCollection } from "astro:content";
 
 const writingCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        date: z.date(),
-        draft: z.boolean(),
-    })
-})
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    draft: z.boolean(),
+  }),
+});
 
 export const collections = {
-    'writing': writingCollection,
-}
+  writing: writingCollection,
+};
