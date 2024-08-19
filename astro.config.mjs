8b92bc172befcd21b4a57a2b2bc437212921c064
Alex Stephen <astephen2@gmail.com>
2024-08-19 09:37:57 -0700

    rss feed working

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  site: 'https://www.alexstephen.me',
   vite: {
     plugins: [yaml()],
   }
