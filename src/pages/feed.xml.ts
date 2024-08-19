47f14701c4f08f420e4c97981bda480b58114ae2
Alex Stephen <astephen2@gmail.com>
2024-08-19 09:40:16 -0700

    added rss icon

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -1,22 +1,22 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const writing = await getCollection('writing');
+  const writing = await getCollection("writing");
   return rss({
     // `<title>` field in output xml
-    title: 'Alex Stephen',
+    title: "Alex Stephen",
     // `<description>` field in output xml
-    description: 'Alex Stephen\'s Blog',
+    description: "Alex Stephen's Blog",
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#contextsite
     site: context.site,
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
     items: writing.map((post) => ({
-        title: post.data.title,
-        pubDate: post.data.date,
-        link: `/writing/${post.slug}/`
+      title: post.data.title,
+      pubDate: post.data.date,
+      link: `/writing/${post.slug}/`,
     })),
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
